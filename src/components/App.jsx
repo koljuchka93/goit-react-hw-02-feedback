9ae8b990c6d00025cc3dcf907fd7f9293d774bf5
Kateryna <koljuchka93@gmail.com>
2022-10-25 01:15:50 +0300

    add new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  
+    state = {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    };
+
+    countTotalFeedback = () => {
+      const {good, neutral, bad} = this.state;
+      const result = good + neutral + bad;
+      return result;
+      };
+
+    countPositiveFeedbackPercentage = () => {
+      const result = this.countTotalFeedback();
+      const {good} = this.state;
+      const persentage = (good * 100) / result;
+      return Math.round(persentage);
+      };
+
+    onLeaveFeedback = option => {
+      this.setState(state => ({
+        [option]: state[option] + 1,
+      }));
+      }
+
 };
